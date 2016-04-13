dae013435977ea3175c31e6ddc2f120ce88957d9
Yi-An Chang <joy01804@gmail.com>
2016-04-13 10:12:11 -0500

    Add config for local use

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var Discordie = require("discordie");
 var Events = Discordie.Events;
+try {
+  var config = require('./config.json');
+}
+catch(err) {
+}
 
 var client = new Discordie();
 
 client.connect({
-  token: process.env.BOT_TOKEN
+  token: process.env.BOT_TOKEN || config.BOT_TOKEN
 });
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
