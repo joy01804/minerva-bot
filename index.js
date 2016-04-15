b2fe19ab1396f565d4c1062e293c279454058198
Yi-An Chang <joy01804@gmail.com>
2016-04-15 14:19:56 -0500

    Switch to cleverbot (Resolves #8)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var loopback = require('loopback');
 var cfenv = require('cfenv');
+var request = require('request-promise');
 
 try {
   var config = require('./config.json');
@@ -9,20 +10,20 @@ try {
 catch(err) {
 }
 
+
 // API application
 var app = module.exports = loopback();
 var appEnv = cfenv.getAppEnv();
-var port = appEnv.isLocal ? config.port : appEnv.port;
 
 app.use('/api', loopback.rest());
 
-app.listen(port, appEnv.bind, () => {
+app.listen(appEnv.port, appEnv.bind, () => {
   console.log('App server starting on ' + appEnv.url);
 });
 
 // Discord Bot
 var Discordie = require("discordie");
-var analyze = require('./libs/analyze')
+var analyze = require('./libs/analyze');
 var Events = Discordie.Events;
 var client = new Discordie();
 
@@ -32,6 +33,22 @@ client.connect({
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
   console.log("Connected to Discord as: " + client.User.username);
+
+  request.post({
+    url: 'https://cleverbot.io/1.0/create',
+    body: {
+      user: process.env.CLEVERBOT_USER || config.CLEVERBOT_USER,
+      key: process.env.CLEAVERBOT_KEY || config.CLEAVERBOT_KEY,
+      nick: client.User.username
+    },
+    json: true
+  })
+  .then(body => {
+    console.log('Connected to CleverBot with session id: ' + body.nick)
+  })
+  .catch(err => {
+    console.error(err);
+  })
 });
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
--- libs/analyze.js
+++ libs/analyze.js
@@ -2,6 +2,13 @@
 
 var swearjar = require('swearjar');
 var cool = require('cool-ascii-faces');
+var request = require('request-promise');
+
+try {
+  var config = require('../config.json');
+}
+catch(err) {
+}
 
 const no_gif = [11, 12, 44, 45, 53, 56, 58, 66, 67, 76];
 
@@ -19,7 +26,7 @@ function legacy(message) {
   if(message.content.match(/Hatsumi/)) {
     message.channel.sendMessage("Sexy Batman says: Looking for trouble?", true);
   }
-}
+};
 
 function wushu(message) {
   let matches = message.content.match(/(\/\d+)/g);
@@ -33,7 +40,7 @@ function wushu(message) {
       message.channel.uploadFile('./assets/aow' + match + '.gif');
     }
   });
-}
+};
 
 function available(num) {
   if(no_gif.indexOf(num) > -1 || num < 1 || num > 84) {
@@ -50,14 +57,34 @@ function mentions(message, id) {
       .then(po => {
         message.reply(' has been muted for 1 minute for disrespecting the bots.');
         setTimeout(function unmute() {
-          po.delete();
-        }, 60000)
+          po.delete()
+        }, 60000);
       });
 
       return;
     }
+
     if(user.id === id) {
-      message.channel.sendMessage(cool());
+      let regex_id = new RegExp('<@'+id+'>', 'g');
+      let msg = message.content.replace(regex_id, 'bot');
+
+      request.post({
+        url: 'https://cleverbot.io/1.0/ask',
+        body: {
+          user: process.env.CLEVERBOT_USER || config.CLEVERBOT_USER,
+          key: process.env.CLEAVERBOT_KEY || config.CLEAVERBOT_KEY,
+          nick: user.username,
+          text: msg
+        },
+        json: true
+      })
+      .then(body => {
+        message.reply(body.response);
+      })
+      .catch(err => {
+        console.error(err);
+        message.reply(cool());
+      })
       return;
     }
   });
